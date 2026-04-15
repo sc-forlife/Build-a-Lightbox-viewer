1ae757071176751d1909c666a440d2c7e1518be6
sc-forlife <salemsc7@gmail.com>
2026-04-15 18:12:35 +0200

    Completed JS logic , Finishing up CSS

--- learn_web.js
+++ learn_web.js
@@ -1,9 +1,22 @@
 const galleryContainer = document.querySelector(".gallery");
 const lightbox = document.querySelector(".lightbox");
 const lightboxImage = document.getElementById("lightbox-image");
+const closeBtn = document.getElementById("close-btn");
 
 galleryContainer.addEventListener("click", (e) => {
-  lightbox.style.display = "block";
-  lightboxImage.attributes.src = `${e.target.currentSrc}`;
-  console.log(lightboxImage.attributes.src);
+  lightbox.style.display = "flex";
+  console.log(e.target.currentSrc);
+  lightboxImage.attributes.src.value = e.target.currentSrc;
+  console.log();
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target.id !== "lightbox-image") {
+    lightbox.style.display = "none";
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  lightboxImage.attributes.src.value = "-thumbnail";
+  lightbox.style.display = "none";
 });
