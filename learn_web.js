05d747a9bf3f776a67327b0377c6eb784a2328ed
sc-forlife <salemsc7@gmail.com>
2026-04-16 07:33:21 +0200

    Completed Lab Work

--- learn_web.js
+++ learn_web.js
@@ -1,14 +1,32 @@
 const galleryContainer = document.querySelector(".gallery");
+const galleryImages = document.getElementsByClassName("gallery-item");
 const lightbox = document.querySelector(".lightbox");
 const lightboxImage = document.getElementById("lightbox-image");
 const closeBtn = document.getElementById("close-btn");
 
-galleryContainer.addEventListener("click", (e) => {
-  lightbox.style.display = "flex";
-  console.log(e.target.currentSrc);
-  lightboxImage.attributes.src.value = e.target.currentSrc;
-  console.log();
-});
+//full sized images
+const fullSizedImg = {
+  stonehenge: "https://cdn.freecodecamp.org/curriculum/labs/stonehenge.jpg",
+  storm: "https://cdn.freecodecamp.org/curriculum/labs/storm.jpg",
+  trees: "https://cdn.freecodecamp.org/curriculum/labs/trees.jpg",
+};
+
+for (const item of galleryImages) {
+  item.addEventListener("click", () => {
+    //using target src to extract key for fullSizedImg
+    const imageURL = item.attributes.src.value;
+
+    if (imageURL.includes("stonehenge-thumbnail.jpg")) {
+      lightboxImage.attributes.src.value = fullSizedImg[`stonehenge`];
+    } else if (imageURL.includes("storm-thumbnail.jpg")) {
+      lightboxImage.attributes.src.value = fullSizedImg[`storm`];
+    } else if (imageURL.includes("trees-thumbnail.jpg")) {
+      lightboxImage.attributes.src.value = fullSizedImg[`trees`];
+    }
+
+    lightbox.style.display = "flex";
+  });
+}
 
 lightbox.addEventListener("click", (e) => {
   if (e.target.id !== "lightbox-image") {
